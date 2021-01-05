991e0fed28be77827a7b97113ccdbc9d510178f0
Donna Samuel <donnasamuel@Donnas-MacBook-Air.local>
2021-01-05 19:55:53 +0200

    Add NoteListItem and NotesExpanded Components

---
+++ src/Containers/NoteListItem.js
@@ -0,0 +1,14 @@
+import './NoteListItem.css'
+
+function NoteListItem() {
+    return (
+        <div class="p-2">
+            <div id="title">Today's Reflections</div>
+            <div id="summary">Lorem ipsum dolor sit amet, consectetur adipsling emali...</div>
+            <br></br>
+            <div id="date">20/12/2020</div>
+         </div>
+    );
+}
+
+export default NoteListItem
---
+++ src/Containers/NotesExpanded.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+  border: 1px solid #DDDDDD;
+  height: 100vh;
+`;
+
+const NotesExpanded = () => (
+  <Div>
+  </Div>
+)
+
+export default NotesExpanded
